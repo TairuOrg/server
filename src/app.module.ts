4c02de752a63dcd4ca150f38fa3ed4781c2257df
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-05-11 20:37:03 -0400

    add orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
+import { CashierController } from './cashier/cashier.controller';
 
 
 
 
 @Module({
   imports: [],
-  controllers: [AppController, AdminController],
+  controllers: [AppController, AdminController, CashierController],
   providers: [AppService, AdminService],
 })
 export class AppModule {}
