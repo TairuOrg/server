dcbac4dcba4183828c33b3cc1f2328f28c948139
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-05-18 13:25:33 -0400

    chore: ExchangeService added as a dependency of adminService

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -4,15 +4,21 @@ import { UpdateAdminDto } from './dto/update-admin.dto';
 import { UserService } from '@/user/user.service';
 import { PrismaService } from '@/prisma/prisma.service';
 import User from '@/user/dto/user';
+import { ExchangeService } from '@/exchange/exchange.service';
 
 @Injectable()
 export class AdminService {
-  constructor(private readonly user: UserService, private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly user: UserService,
+    private readonly prisma: PrismaService,
+    private readonly currency: ExchangeService,
+  ) {}
   async getDashboardData(id: number): Promise<User> {
+    const income = await this.currency.convertCurrency(1);
+    console.log(income)
     return await this.user.getUser(id);
-
   }
-  
+
   findAll() {
     return `This action returns all admin`;
   }
